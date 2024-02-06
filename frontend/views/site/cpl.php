b05503d94db4e57e6059f5533ca4ce0efdcfbecf
nalurryemelsy <nalurryemelsy20@gmail.com>
2024-02-06 22:04:06 +0700

    cpl

--- frontend/views/site/cpl.php
+++ frontend/views/site/cpl.php
@@ -25,4 +25,45 @@ $this->params['breadcrumbs'][] = $this->title;
             <i class="icons icon-arrow-down font-weight-bold"></i>
         </a>
     </section>
+
+    <div class="container pt-2">
+        <div class="row justify-content-center pt-4 mt-5">
+            <div class="col-lg-12 text-center">
+                <!-- <div class="overflow-hidden">
+                    <h2 class="text-color-primary font-weight-medium positive-ls-3 text-4 mb-0 appear-animation" data-appear-animation="maskUp" data-appear-animation-delay="200">HOW WE WORK</h2>
+                </div> -->
+                <div class="overflow-hidden mb-3">
+                    <h3 class="font-weight-bold text-transform-none text-7 line-height-2 mb-0 appear-animation" data-appear-animation="maskUp" data-appear-animation-delay="400">Capaian Pembelajaran Lulusan (CPL)</h3>
+                </div>
+            </div>
+        </div>
+        <br>
+        <div class="row">
+            <div class="col-md-5 text-justify">
+                <h2 class="text-color-primary font-weight-medium positive-ls-3 text-4 mb-0">Pengetahuan</h2>
+                <ul class="list list-icons list-icons-style-2 list-icons-lg custom-list-icons-icon-size pb-1 mb-3">
+                    <li class="font-weight-semibold text-color-dark"><i class="fas fa-check text-color-dark border-color-grey-1 top-7"></i> Lulusan mampu menjelaskan konsep, teori dan perkembangan ilmu pengetahuan di bidang pendidikan dan ilmu sejarah.</li>
+                    <li class="font-weight-semibold text-color-dark"><i class="fas fa-check text-color-dark border-color-grey-1 top-7"></i> Lulusan mampu menerapkan metode, model, strategi, perencanaan, penilaian, dan evaluasi pembelajaran serta menghasilkan karya di bidang pendidikan dan pembelajaran sejarah.</li>
+                    <li class="font-weight-semibold text-color-dark"><i class="fas fa-check text-color-dark border-color-grey-1 top-7"></i> Lulusan mampu menerapkan dan melakukan penelitian dan karya ilmiah dalam bidang pendidikan sejarah dan ilmu sejarah (historiografi).</li>
+                </ul>
+                <h2 class="text-color-primary font-weight-medium positive-ls-3 text-4 mb-0">Sikap</h2>
+                <ul class="list list-icons list-icons-style-2 list-icons-lg custom-list-icons-icon-size pb-1 mb-3">
+                    <li class="font-weight-semibold text-color-dark"><i class="fas fa-check text-color-dark border-color-grey-1 top-7"></i> Lulusan dapat menunjukkan sikap percaya diri, kepribadian positif, kemampuan bekerja mandiri dan kelompok, serta komitmen profesional terhadap etika dan profesi.</li>
+                </ul>
+            </div>
+            <div class="col-md-2"></div>
+            <div class="col-md-5 text-justify">
+                <h2 class="text-color-primary font-weight-medium positive-ls-3 text-4 mb-0">Keterampilan Generik</h2>
+                <ul class="list list-icons list-icons-style-2 list-icons-lg custom-list-icons-icon-size pb-1 mb-3">
+                    <li class="font-weight-semibold text-color-dark"><i class="fas fa-check text-color-dark border-color-grey-1 top-7"></i> Lulusan mampu merancang pembelajaran sejarah di sekolah menengah secara logis, kritis, sistematis, dan inovatif terkait pembelajaran dan pendidikan sejarah.</li>
+                    <li class="font-weight-semibold text-color-dark"><i class="fas fa-check text-color-dark border-color-grey-1 top-7"></i> Lulusan mampu mengajar secara efektif dengan memanfaatkan teknologi informasi secara inovatif dan relevan dalam bidang pendidikan, pembelajaran dan penelitian sejarah.</li>
+                </ul>
+                <h2 class="text-color-primary font-weight-medium positive-ls-3 text-4 mb-0">Pengetahuan</h2>
+                <ul class="list list-icons list-icons-style-2 list-icons-lg custom-list-icons-icon-size pb-1 mb-3">
+                    <li class="font-weight-semibold text-color-dark"><i class="fas fa-check text-color-dark border-color-grey-1 top-7"></i> Lulusan mampu menerapkan prinsip Berpikir Historis dalam pembelajaran sejarah.</li>
+                    <li class="font-weight-semibold text-color-dark"><i class="fas fa-check text-color-dark border-color-grey-1 top-7"></i> Lulusan mampu memecahkan permasalahan berdasarkan perspektif sejarah dan mampu menulis karya ilmiah yang memperkaya historiografi Indonesia.</li>
+                </ul>
+            </div>
+        </div>
+    </div>
 </div>
