7c430ac0be238436ec205c1d1bb3037f7f84565b
Nur Addina <nuraddina563@gmail.com>
2024-02-11 17:06:12 +0700

    nothing

--- backend/views/layouts/main.php
+++ backend/views/layouts/main.php
@@ -225,34 +225,34 @@ AppAsset::register($this);
       <div class="pull-right hidden-xs">
         <b>Version</b> 2.3.0
       </div>
-      <strong>Copyright &copy; 2024 <a href="http://almsaeedstudio.com">Almsaeed Studio</a>.</strong> All rights reserved.
+      <strong>Copyright &copy; 2024 <a href="#>Departemen Sejarah</a>.</strong> All rights reserved.
     </footer>
 
     <!-- Control Sidebar -->
-    <aside class="control-sidebar control-sidebar-dark">
-      <!-- Create the tabs -->
-      <ul class="nav nav-tabs nav-justified control-sidebar-tabs">
-        <li><a href="#control-sidebar-home-tab" data-toggle="tab"><i class="fa fa-home"></i></a></li>
-        <li><a href="#control-sidebar-settings-tab" data-toggle="tab"><i class="fa fa-gears"></i></a></li>
-      </ul>
-      <!-- Tab panes -->
-      <div class="tab-content">
-        <!-- Home tab content -->
-        <div class="tab-pane" id="control-sidebar-home-tab">
-          <h3 class="control-sidebar-heading">Recent Activity</h3>
+    <aside class=" control-sidebar control-sidebar-dark">
+          <!-- Create the tabs -->
+          <ul class="nav nav-tabs nav-justified control-sidebar-tabs">
+            <li><a href="#control-sidebar-home-tab" data-toggle="tab"><i class="fa fa-home"></i></a></li>
+            <li><a href="#control-sidebar-settings-tab" data-toggle="tab"><i class="fa fa-gears"></i></a></li>
+          </ul>
+          <!-- Tab panes -->
+          <div class="tab-content">
+            <!-- Home tab content -->
+            <div class="tab-pane" id="control-sidebar-home-tab">
+              <h3 class="control-sidebar-heading">Recent Activity</h3>
 
 
 
-        </div><!-- /.tab-pane -->
-        <!-- Stats tab content -->
-        <div class="tab-pane" id="control-sidebar-stats-tab">Stats Tab Content</div><!-- /.tab-pane -->
-        <!-- Settings tab content -->
+            </div><!-- /.tab-pane -->
+            <!-- Stats tab content -->
+            <div class="tab-pane" id="control-sidebar-stats-tab">Stats Tab Content</div><!-- /.tab-pane -->
+            <!-- Settings tab content -->
 
-      </div>
-    </aside><!-- /.control-sidebar -->
-    <!-- Add the sidebar's background. This div must be placed
+          </div>
+          </aside><!-- /.control-sidebar -->
+          <!-- Add the sidebar's background. This div must be placed
            immediately after the control sidebar -->
-    <div class="control-sidebar-bg"></div>
+          <div class="control-sidebar-bg"></div>
   </div><!-- ./wrapper -->
 
   <!-- jQuery 2.1.4 -->
